bafacf7347c11c2dfc7eab2728b9284018364e9f
mukunzi10 <mukunzigustavus@gmail.com>
2022-05-24 23:17:53 -0700

    session two

---
+++ session2.js
@@ -0,0 +1,37 @@
+var userName = ''
+'Gustave' ? console.log("Hello, Gustave") :
+console.log('Hello!');
+var userQuestion = 'Am i good at klab?'
+console.log('Am i good at nitrotype?': userQuestion);
+const randomNumber = Math.floor(Math.random() * 8);
+let = ''
+switch (eightBall) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+   eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+}
+console.log(`The eight ball answered:
+ ${eightBall}`);
+var eightBall = ''
+console.log(eightBall);
